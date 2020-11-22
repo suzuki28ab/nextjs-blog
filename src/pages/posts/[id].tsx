21ccb8e8bbf549c4b68b1269f67e77a30bcfe55b
suzuki28ab <suzuki28ab@yahoo.co.jp>
2020-11-22 14:07:34 +0900

    [add]absolute path

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import Layout from '@/components/layout'
+import { getAllPostIds, getPostData } from '@/lib/posts'
 import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+import Date from '@/components/date'
+import utilStyles from '@/styles/utils.module.css'
 import { GetStaticProps, GetStaticPaths } from 'next'
 
 export default function Post({
